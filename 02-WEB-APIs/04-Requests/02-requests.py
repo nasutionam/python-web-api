10e92c510388d210731ec87951a857f56dfb569c
Muhammad Asri Nasution <nasution.nam@gmail.com>
2019-11-08 21:46:56 +0700

    django initiation

--- 02-WEB-APIs/04-Requests/02-requests.py
+++ 02-WEB-APIs/04-Requests/02-requests.py
@@ -6,7 +6,7 @@ def main():
     payload = {"base": "USD", "symbols": "SEK"}
     response = requests.get("https://api.exchangeratesapi.io/latest",
                             params=payload)
-    
+  
     if response.status_code != 200:
         print("Status Code: ", response.status_code)
         raise Exception("There was an error!")
