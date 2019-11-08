2aea0f4246e086175824194b77db39a2a1ea446f
Muhammad Asri Nasution <nasution.nam@gmail.com>
2019-11-08 22:07:49 +0700

    added Product and Manufacture class

--- onlinestore/products/models.py
+++ onlinestore/products/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Manufacture(models.Model):
+    name = models.CharField(max_length=120)
+    location = models.CharField(max_length=120)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    manufacture = models.ForeignKey(Manufacture, on_delete=models.CASCADE, related_name='products')
+    name = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    photo = models.ImageField(blank=True, null=True)
+    price = models.FloatField()
+    shipping_cost = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.name
