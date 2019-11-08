051710543ef5edb7190aa7c01702b751511d64c7
Muhammad Asri Nasution <nasution.nam@gmail.com>
2019-11-08 21:29:06 +0700

    testing request header

--- 02-WEB-APIs/04-Requests/00-requests.py
+++ 02-WEB-APIs/04-Requests/00-requests.py
@@ -4,9 +4,9 @@ def main():
     response = requests.get("http://www.google.com")
     # response = requests.get("http://www.google.com/random-address/")
     print("Status Code: ", response.status_code)
-    # print("Headers: ", response.headers)
+    print("Headers: ", response.headers)
     # print("'Content-Type': ", response.headers['Content-Type'])
-    print("Content: ", response.text)
+    #print("Content: ", response.text)
 
 
 if __name__ == "__main__":
